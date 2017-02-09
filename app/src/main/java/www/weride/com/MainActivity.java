9ff06b2fe496b95158e5a7c801441952c851875e
bryanherrera72 <bryan.herrera72@gmail.com>
2017-02-09 10:28:18 -0800

    initial commit, PERMISSIONS NOT WORKING.

---
+++ app/src/main/java/www/weride/com/MainActivity.java
@@ -0,0 +1,161 @@
+package www.weride.com;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import www.weride.com.fragments.GroupFragment;
+import www.weride.com.fragments.MapFragment;
+
+public class MainActivity extends AppCompatActivity implements MapFragment.OnFragmentInteractionListener,
+                                                                GroupFragment.OnFragmentInteractionListener{
+    private DrawerLayout mainDrawer;
+    private Toolbar toolbar;
+    private NavigationView navDrawer;
+
+    private FragmentManager fragmentManager;
+    private ActionBarDrawerToggle drawerToggle;
+    private MapFragment map;
+    private static final int INITIAL_REQUEST= 1337;
+    private static final String[] LOCATION_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(!(canAccessLocation())){
+            grabPermissions(LOCATION_PERMS, INITIAL_REQUEST+3);
+        }
+        setContentView(R.layout.activity_main);
+        //prepare and set toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //set the drawer layout inside the main layout
+        mainDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navDrawer = (NavigationView) findViewById(R.id.navView);
+        setupDrawerContent(navDrawer);
+        drawerToggle = setupDrawerToggle();
+
+        mainDrawer.addDrawerListener(drawerToggle);
+        if(!(canAccessLocation())){
+            grabPermissions(LOCATION_PERMS, INITIAL_REQUEST+3);
+        }
+
+        //set the first fragment.
+        fragmentManager = getSupportFragmentManager();
+        try{map =  MapFragment.class.newInstance();}
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        fragmentManager.beginTransaction().replace(R.id.flContent, map).commit();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle(){
+        return new ActionBarDrawerToggle(this, mainDrawer,toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+    //Setup the drawer content within the nav view
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+
+
+        });
+    }
+    public void selectDrawerItem(MenuItem item){
+        Fragment frag = null;
+        Class fragmentClass;
+        switch(item.getItemId()){
+            case R.id.map_fragment:
+                fragmentClass = MapFragment.class;
+                break;
+            case R.id.group_fragment:
+                fragmentClass = GroupFragment.class;
+                break;
+            default:
+                fragmentClass = MapFragment.class;
+        }
+        try{
+            if(item.getItemId() == R.id.map_fragment){
+                frag = map;
+            }
+            else {
+                frag = (Fragment) fragmentClass.newInstance();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, frag).commit();
+
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        mainDrawer.closeDrawers();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                mainDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+
+    }
+    @TargetApi(Build.VERSION_CODES.M)
+    private void grabPermissions(String[] perms, int request){
+        requestPermissions(LOCATION_PERMS, INITIAL_REQUEST+3);
+    }
+    private boolean canAccessLocation(){
+        return (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean hasPermission(String perm){
+        return(PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+
+    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/www/weride/com/fragments/MapFragment.java
@@ -0,0 +1,155 @@
+package www.weride.com.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mapzen.android.graphics.MapView;
+import com.mapzen.android.graphics.MapzenMap;
+import com.mapzen.android.graphics.OnMapReadyCallback;
+import com.mapzen.android.graphics.model.BubbleWrapStyle;
+import com.mapzen.android.graphics.model.CameraType;
+import com.mapzen.pelias.widget.PeliasSearchView;
+
+import www.weride.com.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MapFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends com.mapzen.android.graphics.MapFragment implements OnMapReadyCallback{
+    PeliasSearchView searchView;
+    MapzenMap map;
+    private boolean enableLocationOnResume = false;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        searchView = new PeliasSearchView(this.getContext());
+        ActionBar.LayoutParams layoutParams =
+                new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View map = inflater.inflate(R.layout.fragment_map, container, false);
+        MapView mapview = (MapView) map.findViewById(R.id.fragment_map);
+
+        mapview.getMapAsync(new BubbleWrapStyle(), this);
+        return map;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (enableLocationOnResume) {
+           map.setMyLocationEnabled(true);
+        }
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (map.isMyLocationEnabled()) {
+            map.setMyLocationEnabled(false);
+            enableLocationOnResume = true;
+        }
+    }
+    @Override
+    public void onMapReady(MapzenMap mapzenMap) {
+        //set the current instance of the map to this "READY" map
+        //allows access to it throughout the current fragment instance.
+        MapFragment.this.map = mapzenMap;
+        //set some initial configs
+        mapzenMap.setCameraType(CameraType.ISOMETRIC);
+        mapzenMap.setMyLocationEnabled(true);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
