092a42952cfb86fda8aaac8a3e31a464c7572b2d
bryanherrera72 <bryan.herrera72@gmail.com>
2017-02-23 09:58:47 -0800

    added splash screen for loading configs

---
+++ app/src/main/java/www/weride/com/activities/SplashActivity.java
@@ -0,0 +1,24 @@
+package www.weride.com.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import www.weride.com.MainActivity;
+import www.weride.com.R;
+/*
+* This activity will load initial configs for the app.
+* Also gives the user something nice to look at while they wait :)
+* */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+        finish();
+    }
+}
