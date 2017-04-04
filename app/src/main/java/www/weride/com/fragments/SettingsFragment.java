8b721c3b311a1f69a1c9f969cfba4a94af00f896
Kaz-adr <akzadr@gmail.com>
2017-04-04 01:17:35 -0700

    repurposed settings fragment to be notifications fragment

--- app/src/main/java/www/weride/com/fragments/SettingsFragment.java
+++ app/src/main/java/www/weride/com/fragments/SettingsFragment.java
@@ -4,11 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import www.weride.com.R;
+import www.weride.com.classes.NoteInfo;
+import www.weride.com.classes.NoteListAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +34,11 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    //extra initializers
+    private List<NoteInfo> li;
+    private RecyclerView notelist;
+    private NoteListAdapter noteAdapter;
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,7 +77,24 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        li = getData();
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        notelist = (RecyclerView) view.findViewById(R.id.note_list);
+        noteAdapter = new NoteListAdapter(getActivity(), li);
+        notelist.setAdapter(noteAdapter);
+        notelist.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        return view;
+    }
+
+    public static List<NoteInfo> getData(){
+        List<NoteInfo> data = new ArrayList<>();
+        String[] titles = {"join me", "on an adventure", "let's go"};
+        for(int i = 0; i < titles.length; i++){
+            NoteInfo current = new NoteInfo();
+            current.name = titles[i];
+            data.add(current);
+        }
+        return data;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
